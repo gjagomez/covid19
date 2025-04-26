731a522d53ad4d5d6045a0032fe02ad9b3000b43
OscarM10362 <omonroym1@miumg.edu.gt>
2025-04-25 22:04:01 -0600

    Logica para estructura de Json del servicio web, logica para el consumo de servicio web

---
+++ src/main/java/util/ApiResponse.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.util.List;
+
+public class ApiResponse<T> {
+    private List<T> data;
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+}
